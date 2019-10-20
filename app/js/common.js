4f5cdb782607f92d68866ce288503506b228d83e
Azamat Chynasylov <azakgcool@gmail.com>
2019-10-20 23:53:32 +0600

    End Laguna

--- app/js/common.js
+++ app/js/common.js
@@ -30,5 +30,41 @@ $(function () {
 
 	});
 
+	$("a[href='#top']").click(function () {
+		$("html, body").animate({
+			scrollTop: 0
+		}, "slow");
+		return false;
+	});
+
+	$(window).scroll(function () {
+
+		var height = $(document).scrollTop();
+		if (height > 300) {
+			$(".sroll-up").fadeIn(5);
+		} else {
+			$(".sroll-up").fadeOut(5);
+		}
+	});
+
+	function heightses() {
+		//$(".cause-items").height('auto').equalHeights();
+		$(".why-we-content-text p").height('auto').equalHeights();
+		$(".our-mission-count-text p").height('auto').equalHeights();
+	}
+
+	$(window).resize(function () {
+		heightses();
+	});
+
+	$('.our-family-items').slick({
+		lazyLoad: 'ondemand',
+		centerMode: true,
+		slidesToShow: 3,
+		autoplay: true,
+		autoplaySpeed: 2000,
+		infinite: true,
+
+	});
 
 });
